14caf76a549b216add960375c961881d1cc7d6aa
dart <dart@Tigra.(none)>
2013-02-05 23:17:24 +0100

    firstapp initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstapp::Application.config.secret_token = '24201b1922b961d6ac5a7ab641a70cc96e0e69009f924b7cd482ce2c2931ba60c8770e39d3e0a92fab12b01daa000f306f0a70ab19aa04745e7f01aea54bad18'
